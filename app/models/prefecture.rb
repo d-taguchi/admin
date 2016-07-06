63384014170d855da09c928bb111439b3aef1121
D.Taguchi <d-taguchi@ecvoo.net>
2016-07-06 04:28:37 +0000

    Relations resetting

--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -1,2 +1,5 @@
 class Prefecture < ActiveRecord::Base
+    # リレーション設定
+    # 1<user_datum> : 1 <pref_id>
+    belongs_to :user_datum
 end
--- app/models/user_datum.rb
+++ app/models/user_datum.rb
@@ -3,4 +3,6 @@ class UserDatum < ActiveRecord::Base
     # リレーション設定
     # 1<user> : 1 <user_datum>
     belongs_to :user, foreign_key: "user_id"
+
+    has_one :prefecture, primary_key: 'pref_id', foreign_key: 'id'
 end
